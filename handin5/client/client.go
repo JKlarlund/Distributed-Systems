3b1a1e35f5658d01a40e7bee662739f0db89a6e7
Phillip <phir@itu.dk>
2024-11-19 11:32:08 -0500

    Added the RPC methods to the client file

--- handin5/client/client.go
+++ handin5/client/client.go
@@ -13,6 +13,33 @@ type Client struct {
 	ID    int32
 }
 
+func (c Client) Bid(ctx context.Context, in *pb.BidRequest, opts ...grpc.CallOption) (*pb.BidResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c Client) Result(ctx context.Context, in *pb.ResultRequest, opts ...grpc.CallOption) (*pb.ResultResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c Client) AuctionStream(ctx context.Context, opts ...grpc.CallOption) (grpc.BidiStreamingClient[pb.AuctionMessage, pb.AuctionMessage], error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c Client) Join(ctx context.Context, in *pb.JoinRequest, opts ...grpc.CallOption) (*pb.JoinResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c Client) Leave(ctx context.Context, in *pb.LeaveRequest, opts ...grpc.CallOption) (*pb.LeaveResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+var client Client
+
 func main() {
 	conn, err := grpc.DialContext(context.Background(), "localhost:1337", grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
@@ -22,5 +49,16 @@ func main() {
 
 	client := pb.NewAuctionServiceClient(conn)
 
-	client.
+	response, err := client.Join(context.Background(), &pb.JoinRequest{})
+	if err != nil {
+		log.Printf("User failed to join the AuctionStream")
+	}
+
+	stream, err := client.AuctionStream(context.Background())
+	if err == nil {
+		log.Printf("Connection was established as user: %d", response.UserID)
+
+	}
+	client = Client{ID: response.UserID, Clock: Clock.InitializeLClock(2)}
+
 }
