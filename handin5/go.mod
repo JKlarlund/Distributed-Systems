df0e4dc236144fe5c43ee83a5a7f0077e4b1529a
JKlarlund <jonathanklarlund@outlook.com>
2024-11-20 11:18:37 +0100

    Some updates to rpc aswell, missing some stuff <3

--- handin5/go.mod
+++ handin5/go.mod
@@ -3,25 +3,16 @@ module github.com/JKlarlund/Distributed-Systems/tree/main/handin5
 go 1.23.0
 
 require (
+	google.golang.org/genproto v0.0.0-20241118233622-e639e219e697
 	google.golang.org/grpc v1.68.0
 	google.golang.org/protobuf v1.35.2
 )
 
 require (
-	golang.org/x/net v0.29.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/text v0.18.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
-)
-
-require (
-	google.golang.org/grpc v1.68.0
-	google.golang.org/protobuf v1.35.2
-)
-
-require (
-	golang.org/x/net v0.29.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/text v0.18.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	cloud.google.com/go/iam v1.2.2 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20241113202542-65e8d215514f // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241113202542-65e8d215514f // indirect
 )
--- handin5/server/server.go
+++ handin5/server/server.go
@@ -17,6 +17,10 @@ type Server struct {
 	bidders     map[int32](pb.AuctionService_AuctionStreamServer)
 }
 
+/*
+Sets up a server node. You must specify via flag whether it's the primary or not, by calling it with flag -isPrimary true/false
+Server nodes reserve ports 5000 and 5001.
+*/
 func main() {
 	isPrimary := *flag.Bool("isPrimary", false, "Is the node the primary server?")
 	flag.Parse()
@@ -34,39 +38,55 @@ func main() {
 
 	pb.RegisterAuctionServiceServer(server, &Server{
 		selfAddress: selfAddress,
-		Clock: Clock.InitializeLClock(0),
-		ID: id,
-		bidders: make(map[int32](pb.AuctionService_AuctionStreamServer)),
+		Clock:       Clock.InitializeLClock(0),
+		ID:          id,
+		bidders:     make(map[int32]pb.AuctionService_AuctionStreamServer),
 	})
 
 }
 
+/*
+ */
 func (s *Server) AuctionStream(stream pb.AuctionService_AuctionStreamServer) error {
 	msg, err := stream.Recv()
+	s.Clock.ReceiveEvent(msg.Timestamp)
+
 	if err != nil {
 		log.Println(err.Error())
 	}
 	if s.bidders[msg.UserID] == nil {
+		log.Printf("User %d has joined the stream at time with bid %d")
 		s.bidders[msg.UserID] = stream
 	}
 
-	for{
-
+	for {
+		msg, _ := stream.Recv()
+		s.Clock.ReceiveEvent(msg.Timestamp)
+		s.broadcastBid(&pb.BidRequest{
+			Amount:   msg.Bid,
+			BidderId: msg.UserID,
+		})
 	}
-
 }
 
-func (s *Server) broadcastBid(bidRequest pb.BidRequest){
+func (s *Server) broadcastBid(bidRequest *pb.BidRequest) {
 
-	bid := pb.AuctionStream{
-		HighestBid: 0,
-		AuctionEnded: false,
-		HighestBidder: "",
-		Message: bidRequest.
+	var broadcastMessage string = fmt.Sprintf("User %d has bid amount %d at time xx", bidRequest.BidderId, bidRequest.Amount)
+
+	bid := pb.AuctionMessage{
+		Bid:       bidRequest.Amount,
+		Timestamp: s.Clock.SendEvent(),
+		UserID:    bidRequest.BidderId,
+		Content:   broadcastMessage,
 	}
 
-	for _, stream  := range s.bidders {
-		stream.Send()
-	}
+	log.Printf(broadcastMessage)
 
+	for _, stream := range s.bidders {
+		stream.Send(&bid)
+	}
+}
+
+func formatBidMessage(message *pb.AuctionMessage) string {
+	return fmt.Sprintf("Server has received request from user %d to bid %d at time %d", message.UserID, message.Bid, message.Timestamp)
 }
